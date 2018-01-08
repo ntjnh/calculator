ad94f75d3e5e5ae691ab1af9e66c5feed5d59973
Nate Jonah <nate@natejonah.com>
2018-01-08 02:01:11 +0000

    Add calculation feature

--- js/app.js
+++ js/app.js
@@ -18,7 +18,14 @@ const numberButtons = document.getElementsByClassName("number");
 // Add click event to each button
 for (let i = 0; i < numberButtons.length; i++) {
   numberButtons[i].addEventListener("click", () => {
-
+    // With each button press, add the number to num string
+    num += numberButtons[i].value;
+    // Display num string so user can see their input
+    answer.textContent = num;
+    // With each button press, build the calculation string
+    calc += numberButtons[i].value;
+    // Display calculation string
+    showCalc.textContent = calc;
   });
 }
 
@@ -28,14 +35,35 @@ const operatorButtons = document.getElementsByClassName("operator");
 // Add click event to each operator button
 for (let j = 0; j < operatorButtons.length; j++) {
   operatorButtons[j].addEventListener("click", () => {
-
+    // Add to, subtract from, multiply by, or divide by num and add that to result
+    switch(operatorButtons[j].value) {
+      case "+":
+        result += Number(num);
+        break;
+      case "-":
+        result -= Number(num);
+        break;
+      case "*":
+        result *= Number(num);
+        break;
+      case "/":
+        result /= Number(num);
+        break;
+    }
+    num = "";
+    // Add the operator to the calculation string
+    calc += operatorButtons[j].value;
+    showCalc.textContent = calc;
   });
 }
 
 // Equals
 const equals = document.getElementById("equals");
 equals.addEventListener("click", () => {
-
+  num = "";
+  calc += "=" + result;
+  answer.textContent = result;
+  showCalc.textContent = calc;
 });
 
 // AC button - clears calculations and sets the screen element values back to 0
