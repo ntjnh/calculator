8a5d05d1cf6324dbfde03bdcdec90b741568a774
Nate Jonah <nate@natejonah.com>
2018-01-08 01:07:06 +0000

    Add event listeners to buttons

--- js/app.js
+++ js/app.js
@@ -15,14 +15,37 @@ let result = 0;
 // Number buttons
 const numberButtons = document.getElementsByClassName("number"); 
 
+// Add click event to each button
+for (let i = 0; i < numberButtons.length; i++) {
+  numberButtons[i].addEventListener("click", () => {
+    
+  });
+}
+
 // Operator buttons
 const operatorButtons = document.getElementsByClassName("operator");
 
+// Add click event to each operator button
+for (let j = 0; j < operatorButtons.length; j++) {
+  operatorButtons[j].addEventListener("click", () => {
+    
+  });
+}
+
 // Equals
 const equals = document.getElementById("equals");
+equals.addEventListener("click", () => {
+  
+});
 
 // AC button - clears calculations and sets the screen element values back to 0
 const ac = document.getElementById("ac");
+ac.addEventListener("click", () => {
+  
+});
 
 // CE button - clears last entry
 const ce = document.getElementById("ce");
+ce.addEventListener("click", () => {
+  
+});
