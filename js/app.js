bfc32f21a68e5700579c94a0632fbe7c0392e5c4
Nate Jonah <nate@natejonah.com>
2018-01-29 22:48:39 +0000

    Add event listener for buttons

---
+++ js/app.js
@@ -0,0 +1,32 @@
+// Keypad
+const keypad = document.querySelector(".keypad");
+
+keypad.addEventListener("click", e => {
+  switch(e.target.value) {
+    case "ac": clearAll();
+      break;
+    case "ce": clearPrev();
+      break;
+    case "/":
+    case "*":
+    case "-":
+    case "+":
+      operator(e.target.value);
+      break;
+    case "=": equals();
+      break;
+    case ".":
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      buildNumber(e.target.value);
+      break;
+  }
+});
