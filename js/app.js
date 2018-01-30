c46f6602b672108f4fd7f69a8031131a4ca22f12
Nate Jonah <nate@natejonah.com>
2018-01-30 17:28:50 +0000

    Bug fix: Edit clearPrev() if statement

--- js/app.js
+++ js/app.js
@@ -162,11 +162,11 @@ function clearAll() {
 // CE button function
 function clearPrev() {
   if (num.length === 1) {
-    num = "";
-    answer.textContent = "0";
+    num = "0";
+    calc = "0";
   } else if (num.length > 1) {
-    num = num.substring(0, num.length - 1); // fix this!! still works if num.length is 1
-    calc = calc.substring(0, calc.length - 1); // fix this!! still works if num.length is 1
+    num = num.substring(0, num.length - 1);
+    calc = calc.substring(0, calc.length - 1);
   }
   answer.textContent = num;
   calculation.textContent = calc;
